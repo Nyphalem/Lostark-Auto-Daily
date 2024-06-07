543f4a49710bddffb3fb9c3ce799998e7cf8143a
nyphalem <zhangzy19@163.com>
2024-06-07 08:15:33 +0800

    sync all files, this repo just for thsy

---
+++ personalCharacters.py
@@ -0,0 +1,47 @@
+characters = [
+    {
+        "index": 0,
+        "class": "bard",
+        "ilvl-aor": 1560,
+    },
+    {
+        "index": 1,
+        "class": "bard",
+        "ilvl-aor": 1445,
+    },
+    {
+        "index": 2,
+        "class": "bard",
+        "ilvl-aor": 1445,
+    },
+    {
+        "index": 3,
+        "class": "bard",
+        "ilvl-aor": 1415,
+    },
+    {
+        "index": 4,
+        "class": "bard",
+        "ilvl-aor": 1415,
+    },
+    {
+        "index": 5,
+        "class": "holyknight",
+        "ilvl-aor": 1415,
+    },
+    {
+        "index": 6,
+        "class": "demonic",
+        "ilvl-aor": 1415,
+    },
+    {
+        "index": 7,
+        "class": "slayer",
+        "ilvl-aor": 1370,
+    },
+    {
+        "index": 8,
+        "class": "sorceress",
+        "ilvl-aor": 500,
+    },
+]
